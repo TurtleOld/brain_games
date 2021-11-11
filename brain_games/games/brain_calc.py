cfd62dfa3c5c84c9a6f5a28ed3d86a196dbf8c73
Alexander <dev@pavlovteam.ru>
2021-11-11 20:30:29 +0300

    Update brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,8 +1,8 @@
 """Game: Calculator."""
 from random import choice, randint
 
-start_number = 1
-end_number = 100
+START_NUMBER = 1
+END_NUMBER = 100
 GAME_DESCRIPTION = 'What is the result of the expression?'
 
 
@@ -11,9 +11,8 @@ def get_game_round():
     random_num1 = randint(start_number, end_number)
     random_num2 = randint(start_number, end_number)
     symbols = ['+', '-', '*']
-    random_symbols = choice(symbols)
+    random_symbol = choice(symbols)
     question = '{0} {1} {2}'.format(random_num1, random_symbols, random_num2)
-    answer = ''
     if random_symbols == '+':
         answer = random_num1 + random_num2
     elif random_symbols == '-':

8ccc654453dcd827d6f3ad719bcca52ed150e402
Alexander <dev@pavlovteam.ru>
2021-11-13 14:17:52 +0300

    bug fix

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -7,6 +7,7 @@ GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers'
 
 
 def is_gcd(random_number1, random_number2):
+    """Получение наибольшего общего делителя."""
     while random_number1 != 0 and random_number2 != 0:
         if random_number1 > random_number2:
             random_number1 %= random_number2

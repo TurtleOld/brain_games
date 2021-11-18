af471feee0ba459895101c2ad3bcc21080f529fc
Alexander <dev@pavlovteam.ru>
2021-11-18 18:29:02 +0300

    Update brain_prime.py

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -9,14 +9,14 @@ END_NUMBER = 100
 
 def is_prime(random_number):
     """Проверка на простое число."""
-    number = 0
+
     for i in range(2, random_number // 2):
         if random_number % i == 0:
-            number = 1
-    if number <= 0:
+            return False
+
         return True
-    else:
-        return False
+
+
 
 
 def get_game_round():

efe7a1fdb01bf78f8dca77bc69cfa90936e0f146
Alexander <dev@pavlovteam.ru>
2021-11-22 14:50:45 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -5,7 +5,7 @@ from brain_games.games import brain_gcd
 
 def main():
     """Запускаем игру по нахождению наибольшего общего делителя."""
-    get_engine_of_games(brain_gcd)
+    run_engine_of_games(brain_gcd)
 
 
 if __name__ == '__main__':
